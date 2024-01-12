78688537e52346c5d574216acddcd536687d7314
Mads U. Jensen <MADS0LADEN@users.noreply.github.com>
2024-01-12 12:54:09 +0100

    0625 resolution

--- esp/read_TCN75A.py
+++ esp/read_TCN75A.py
@@ -5,27 +5,31 @@ from machine import Pin, SoftI2C
 # Define I2C bus
 i2c = SoftI2C(scl=Pin(9), sda=Pin(8))
 
-# TCN75A address, 0x48(72)
+# TCN75A addresses
 TCN75A_ADDR = 0x48
 TCN75A_ADDR2 = 0x49
 
-print(i2c.scan())
+CONFIG_REG_ADDR = 0x01
+CONFIG_VALUE = 0x60
 
-time.sleep(1)
-
+i2c.writeto_mem(TCN75A_ADDR, CONFIG_REG_ADDR, bytearray([CONFIG_VALUE]))
+i2c.writeto_mem(TCN75A_ADDR2, CONFIG_REG_ADDR, bytearray([CONFIG_VALUE]))
 
 while True:
-    # Read data back from 0x00(0), 2 bytes
-    # temp MSB, temp LSB
+    # Read data from TCN75A_ADDR
     data = i2c.readfrom_mem(TCN75A_ADDR, 0x00, 2)
-    data2 = i2c.readfrom_mem(TCN75A_ADDR2, 0x00, 2)
-
-    # Convert the data to 13-bits
     temp = ((data[0] << 8) | (data[1] & 0xFF)) / 128
+
+    # Read data from TCN75A_ADDR2
+    data2 = i2c.readfrom_mem(TCN75A_ADDR2, 0x00, 2)
     temp2 = ((data2[0] << 8) | (data2[1] & 0xFF)) / 128
 
-    # Output data to console
-    print("Temperature in Celsius is : %.4f C, %.2f C" % (temp, temp2))
+    # print(data, data2)
 
-    # Wait for a second before reading the data again
-    time.sleep(0.1)
+    # Output data to console
+    print(
+        ("Temperature in Celsius is: %.2f C, %.2f C" % (temp, temp2)), abs(temp - temp2)
+    )
+
+    # Wait for 0.1 seconds before reading the data again
+    time.sleep(0.05)
